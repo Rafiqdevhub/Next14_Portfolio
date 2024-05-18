7976b3e7a04a912d98b6deea9ebada37297986da
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-05-18 21:23:46 +0500

    created some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        hi
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid/>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
